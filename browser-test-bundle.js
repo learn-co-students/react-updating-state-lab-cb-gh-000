936d0874bb496af9ced7176e193aec38cf0ca325
thomastuts <thomas@thomastuts.com>
2016-08-22 17:30:42 +0200

    Add solution code

--- browser-test-bundle.js
+++ browser-test-bundle.js
@@ -1,10 +1,138 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-},{}],2:[function(require,module,exports){
-"use strict";
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-},{}],3:[function(require,module,exports){
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var React = require('react');
+
+var DigitalClicker = function (_React$Component) {
+  _inherits(DigitalClicker, _React$Component);
+
+  function DigitalClicker() {
+    _classCallCheck(this, DigitalClicker);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(DigitalClicker).call(this));
+
+    _this.state = {
+      timesClicked: 0
+    };
+
+    _this.handleClick = _this.handleClick.bind(_this);
+    return _this;
+  }
+
+  _createClass(DigitalClicker, [{
+    key: 'handleClick',
+    value: function handleClick() {
+      this.setState({
+        timesClicked: ++this.state.timesClicked
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'button',
+        { onClick: this.handleClick },
+        this.state.timesClicked
+      );
+    }
+  }]);
+
+  return DigitalClicker;
+}(React.Component);
+
+module.exports = DigitalClicker;
+
+},{"react":414}],2:[function(require,module,exports){
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var React = require('react');
+
+var YouTubeDebugger = function (_React$Component) {
+  _inherits(YouTubeDebugger, _React$Component);
+
+  function YouTubeDebugger() {
+    _classCallCheck(this, YouTubeDebugger);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(YouTubeDebugger).call(this));
+
+    _this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+
+    _this.handleChangeBitrate = _this.handleChangeBitrate.bind(_this);
+    _this.handleChangeResolution = _this.handleChangeResolution.bind(_this);
+    return _this;
+  }
+
+  _createClass(YouTubeDebugger, [{
+    key: 'handleChangeBitrate',
+    value: function handleChangeBitrate() {
+      this.setState({
+        settings: Object.assign({}, this.state.settings, {
+          bitrate: 12
+        })
+      });
+    }
+  }, {
+    key: 'handleChangeResolution',
+    value: function handleChangeResolution() {
+      this.setState({
+        settings: Object.assign({}, this.state.settings, {
+          video: Object.assign({}, this.state.settings.video, {
+            resolution: '720p'
+          })
+        })
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'button',
+          { className: 'bitrate', onClick: this.handleChangeBitrate },
+          'Change bitrate'
+        ),
+        React.createElement(
+          'button',
+          { className: 'resolution', onClick: this.handleChangeResolution },
+          'Change resolution'
+        )
+      );
+    }
+  }]);
+
+  return YouTubeDebugger;
+}(React.Component);
+
+module.exports = YouTubeDebugger;
+
+},{"react":414}],3:[function(require,module,exports){
 'use strict'
 
 exports.toByteArray = toByteArray
