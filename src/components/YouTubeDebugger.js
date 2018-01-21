c62ab1b6befcc5dfc022577b872765bc291e9725
EC2 Default User <ec2-user@ip-172-31-40-57.us-east-2.compute.internal>
2018-01-21 09:26:07 +0000

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,44 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+    changeBitrate = ()=>{
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+    changeRes = ()=>{
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+    render(){
+        return (
+            <div>
+                <button className="bitrate" onClick={this.changeBitrate}>Change bitrate to 12 ({this.state.settings.bitrate})</button>
+                <button className="resolution" onClick={this.changeRes}>Change to 720p ({this.state.settings.video.resolution})</button>
+            </div>
+        );
+    }
+}
