a079027a6ec080bb26bf3a707e49603668fc81c3
Susanta Biswas <susantasky@gmail.com>
2018-11-20 04:21:11 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      timesClicked:0
+    };
+  }
+  render(){
+    return(
+      <div className="digital-clicker">
+        <button  onClick={()=>{
+          this.setState({
+            timesClicked: this.state.timesClicked + 1});
+        }}>{this.state.timesClicked}
+        </button>
+      </div>
+    );
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,43 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+  }
+
+  render(){
+    return(
+      <div className="youtube-debugger">
+        <button className="bitrate"  onClick={()=>{
+                this.setState({
+                  settings: {...this.state.settings,
+                            bitrate:12}
+                          }, );
+            }
+          }>
+        </button>
+
+        <button className="resolution"  onClick={()=>{
+          this.setState({
+            settings: {...this.state.settings,
+                        video:{resolution:'720p'}
+                      }
+            });
+          }
+        }>
+        </button>
+      </div>
+    );
+  }
+}
